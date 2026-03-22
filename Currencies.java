ea71f9f09e7c2accd9be7d3de5674a2b02eca735
suchit-goled <suchitgoled@gmail.com>
2026-03-22 23:02:50 +0530

    message

---
+++ Currencies.java
@@ -0,0 +1,160 @@
+class Currencies{
+
+
+	public static void main(String[] args){
+		
+		String[] currencies={"Indian Rupee","Pakistani Rupee", "Bangladeshi Taka","Sri Lankan Rupee", "Nepalese Rupee", 
+							"Indonesian Rupiah", "Malaysian Ringgit", "Singapore Dollar","Thai Baht","Philippine Peso","Vietnamese Dong",
+							"South Korean Won","North Korean Won","Mongolian Tögrög","Myanmar Kyat","Cambodian Riel","Lao Kip","Brunei Dollar","Taiwanese Dollar",
+							"US Dollar","Euro","British Pound Sterling","Japanese Yen","Swiss Franc","Canadian Dollar","Australian Dollar","New Zealand Dollar",
+							"Chinese Yuan","Hong Kong Dollar","Macanese Pataca","Aruban Florin","Icelandic Króna","Polish Złoty",
+							"UAE Dirham","Saudi Riyal","Qatari Riyal","Kuwaiti Dinar","Bahraini Dinar","Omani Rial","Iranian Rial","Iraqi Dinar",
+							"Jordanian Dinar","Israeli New Shekel","Lebanese Pound","Syrian Pound","Yemeni Rial","Swedish Krona","Norwegian Krone","Danish Krone",
+							"Czech Koruna","Hungarian Forint","Romanian Leu","Bulgarian Lev","Serbian Dinar","Croatian Kuna","Albanian Lek",
+							"Ukrainian Hryvnia","Belarusian Ruble","Russian Ruble","Turkish Lira","Georgian Lari","Armenian Dram","Azerbaijani Manat",
+							"Moldovan Leu","South African Rand","Nigerian Naira","Kenyan Shilling","Ghanaian Cedi","Ethiopian Birr","Egyptian Pound",
+							"Moroccan Dirham","Tunisian Dinar","Algerian Dinar","Libyan Dinar","Sudanese Pound","South Sudanese Pound","Tanzanian Shilling",
+							"Ugandan Shilling","Rwandan Franc","Burundian Franc","Zambian Kwacha","Zimbabwean Dollar","Botswanan Pula","Namibian Dollar",
+							"Malawian Kwacha","Mozambican Metical","Angolan Kwanza","Malagasy Ariary","Mauritian Rupee","Seychellois Rupee",
+							"Cape Verdean Escudo","Mexican Peso","Brazilian Real","Argentine Peso","Chilean Peso","Colombian Peso","Peruvian Sol",
+							"Venezuelan Bolívar","Uruguayan Peso","Paraguayan Guaraní","Bolivian Boliviano","Costa Rican Colón","Panamanian Balboa",
+							"Cuban Peso","Dominican Peso","Jamaican Dollar","Haitian Gourde","Trinidad and Tobago Dollar","Bahamian Dollar",
+							"Barbadian Dollar","Belize Dollar","Eastern Caribbean Dollar","Guyanese Dollar","Surinamese Dollar","Fijian Dollar",
+							"Papua New Guinean Kina","Solomon Islands Dollar","Vanuatu Vatu","Samoan Tala","Tongan Paʻanga","Kiribati Dollar",
+							"Tuvaluan Dollar","CFA Franc (West African)","CFA Franc (Central African)","CFP Franc","East Timor US Dollar",
+							"Eritrean Nakfa","Bhutanese Ngultrum","Afghan Afghani","Tajikistani Somoni","Uzbekistani Som","Turkmenistani Manat",
+							"Kyrgyzstani Som","Palestinian Pound","Sahrawi Peseta","Transnistrian Ruble","Somaliland Shilling","Abkhazian Apsar",
+							"South Ossetian Ruble","Artsakh Dram","Cook Islands Dollar","Niue Dollar","Falkland Islands Pound","Gibraltar Pound",
+							"Saint Helena Pound","Jersey Pound","Guernsey Pound","Isle of Man Pound","Bermudian Dollar","Cayman Islands Dollar",
+							};
+		int size = currencies.length;
+		System.out.println("The size of the Currencies is :"+size);
+		
+		String reference = currencies[0];
+        System.out.println("0th Position of the currency is: " + reference);
+		
+		reference = currencies[3];
+		System.out.println("3rd Position of the currency is :"+reference);
+		
+		reference = currencies[1];
+		System.out.println("1st Position of the currency is :"+reference);
+		
+		reference = currencies[4];
+		System.out.println("4th Position of the currency is :"+reference);
+		
+		reference = currencies[5];
+		System.out.println("5th Position of the currency is :"+reference);
+		
+		reference = currencies[6];
+		System.out.println("6th Position of the currency is :"+reference);
+		
+		reference = currencies[7];
+		System.out.println("7th Position of the currency is :"+reference);
+		
+		reference = currencies[100];
+		System.out.println("100th Position of the currency is :"+reference);
+		
+		reference = currencies[120];
+		System.out.println("120th Position of the currency is :"+reference);
+		
+		reference = currencies[144];
+		System.out.println("144th Position of the currency is :"+reference);
+		
+		reference = currencies[135];
+		System.out.println("135th Position of the currency is :"+reference);
+		
+		reference = currencies[57];
+		System.out.println("57th Position of the currency is :"+reference);
+		
+		reference = currencies[14];
+		System.out.println("14th Position of the currency is :"+reference);
+		
+		reference = currencies[27];
+		System.out.println("27th Position of the currency is :"+reference);
+		
+		reference = currencies[25];
+		System.out.println("25th Position of the currency is :"+reference);
+		
+		reference = currencies[134];
+		System.out.println("134th Position of the currency is :"+reference);
+		
+		reference = currencies[78];
+		System.out.println("78th Position of the currency is :"+reference);
+		
+		reference = currencies[87];
+		System.out.println("87th Position of the currency is :"+reference);
+		
+		reference = currencies[46];
+		System.out.println("46th Position of the currency is :"+reference);
+		
+		reference = currencies[45];
+		System.out.println("45th Position of the currency is :"+reference);
+		
+		reference = currencies[8];
+		System.out.println("8th Position of the currency is :"+reference);
+		
+		reference = currencies[58];
+		System.out.println("58th Position of the currency is :"+reference);
+
+		reference = currencies[31];
+		System.out.println("31st Position of the currency is :" + reference);
+
+		reference = currencies[42];
+		System.out.println("42nd Position of the currency is :" + reference);
+
+		reference = currencies[63];
+		System.out.println("63rd Position of the currency is :" + reference);
+
+		reference = currencies[74];
+		System.out.println("74th Position of the currency is :" + reference);
+
+		reference = currencies[85];
+		System.out.println("85th Position of the currency is :" + reference);
+
+		reference = currencies[96];
+		System.out.println("96th Position of the currency is :" + reference);
+
+		reference = currencies[107];
+		System.out.println("107th Position of the currency is :" + reference);
+
+		reference = currencies[118];
+		System.out.println("118th Position of the currency is :" + reference);
+
+		reference = currencies[129];
+		System.out.println("129th Position of the currency is :" + reference);
+
+		reference = currencies[140];
+		System.out.println("140th Position of the currency is :" + reference);
+
+		reference = currencies[33];
+		System.out.println("33rd Position of the currency is :" + reference);
+
+		reference = currencies[52];
+		System.out.println("52nd Position of the currency is :" + reference);
+
+		reference = currencies[67];
+		System.out.println("67th Position of the currency is :" + reference);
+
+		reference = currencies[88];
+		System.out.println("88th Position of the currency is :" + reference);
+
+		reference = currencies[109];
+		System.out.println("109th Position of the currency is :" + reference);
+
+		reference = currencies[121];
+		System.out.println("121st Position of the currency is :" + reference);
+
+		reference = currencies[132];
+		System.out.println("132nd Position of the currency is :" + reference);
+
+		reference = currencies[143];
+		System.out.println("143rd Position of the currency is :" + reference);
+
+		reference = currencies[60];
+		System.out.println("60th Position of the currency is :" + reference);
+
+		reference = currencies[91];
+		System.out.println("91st Position of the currency is :" + reference);
+				
+	}
+}
---
+++ Jackets.java
@@ -0,0 +1,50 @@
+class Jackets{
+
+public static void main(String[] values){
+
+
+
+	String[] jacketBrands = {
+    "Nike","Adidas","Puma","Reebok","The North Face","Columbia","Levi's","Tommy Hilfiger","Calvin Klein","Superdry","Jack & Jones"
+	};
+	double size=jacketBrands.length;
+	System.out.println("number of jacket brands are:"+size);
+	
+	String ref=jacketBrands[0];
+	System.out.println("0th position brand name is:"+ref);
+	
+	ref=jacketBrands[1];
+	System.out.println("1st position brand name is:"+ref);
+	
+	ref = jacketBrands[2];
+	System.out.println("2nd position brand name is: " + ref);
+
+	ref = jacketBrands[3];
+	System.out.println("3rd position brand name is: " + ref);
+
+	ref = jacketBrands[4];
+	System.out.println("4th position brand name is: " + ref);
+
+	ref = jacketBrands[5];
+	System.out.println("5th position brand name is: " + ref);
+
+	ref = jacketBrands[6];
+	System.out.println("6th position brand name is: " + ref);
+
+	ref = jacketBrands[7];
+	System.out.println("7th position brand name is: " + ref);
+
+	ref = jacketBrands[8];
+	System.out.println("8th position brand name is: " + ref);
+
+	ref = jacketBrands[9];
+	System.out.println("9th position brand name is: " + ref);
+
+	ref = jacketBrands[10];
+	System.out.println("10th position brand name is: " + ref);
+		
+	
+}
+}
+
+
