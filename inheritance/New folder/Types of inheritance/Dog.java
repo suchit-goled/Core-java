305c375ab4575bfa8a0104563eb0d1d5c7a3695c
suchit-goled <suchitgoled@gmail.com>
2026-04-23 22:42:47 +0530

    multi level inheritance

---
+++ inheritance/New folder/Types of inheritance/Dog.java
@@ -0,0 +1,8 @@
+
+//multilevel inheritance where dog has a parent class named animal and animal has parent class Object
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
