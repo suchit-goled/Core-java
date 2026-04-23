b2bb3b1fbc35eabb169aa291119816d6508c447a
suchit-goled <suchitgoled@gmail.com>
2026-04-23 22:45:37 +0530

    hierarchial inheritance

---
+++ inheritance/New folder/Types of inheritance/Cat.java
@@ -0,0 +1,7 @@
+//in hierarchial the parent class has two subclass here cat and dog class have same parent class Animal
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
+    }
+}
