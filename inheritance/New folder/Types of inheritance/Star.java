c2e73dcfa7b91596d7e6b27fe2b9fb0983bc0803
suchit-goled <suchitgoled@gmail.com>
2026-06-03 00:48:20 +0530

    new

--- inheritance/New folder/Types of inheritance/Star.java
+++ inheritance/New folder/Types of inheritance/Star.java
@@ -6,11 +6,12 @@ public static void main(String[] args){
 	int j;
 	
 	
-	for(i=0;i<n;i++)
-		for(j=0;j<n;j++){
-		System.out.println("*");
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+		System.out.print(" *");
 }
 System.out.println();
+}
 }
 }
 
---
+++ inheritance/New folder/Types of inheritance/Triangle.java
@@ -0,0 +1,14 @@
+class Triangle{
+
+public static void main(String[] args){
+	int i; 
+	int j;
+	for(i=0;i<5;i++){
+		for(j=0;j=i;j++){
+		System.out.println("*");
+	}
+	System.out.println();
+	}
+
+}
+}
