54e9535ef70df763c37fa23c486d71aeaa0d6d75
suchit-goled <suchitgoled@gmail.com>
2026-06-26 02:19:56 +0530

    nested catch and uses of it

---
+++ com/suchit/Exceptions/NestedCatch.java
@@ -0,0 +1,31 @@
+package com.suchit.Exceptions;
+
+
+
+public class NestedCatch {
+
+    public static void main(String[] args){
+        int A[]={30,20,10,40,0};
+        try {
+            int c = A[0] / A[1];
+
+            System.out.println("division is " + c);
+            //nested try and catch block
+            try {
+                System.out.println(A[5]);
+            }
+            //if there is any elements or the loop try to access the extra index of the array assinged it will give ArrayIndexOutOfBound exception which is the next block
+            catch(ArrayIndexOutOfBoundsException e){
+                System.out.println("Index access is high");
+            }
+        }
+        // if there is any arithmetic exception like if denominator is zero it prints the next statement
+        catch(ArithmeticException e){
+            System.out.println("denominator should not be zero");
+
+        }
+        //if there is any elements or the loop try to access the extra index of the array assinged it will give ArrayIndexOutOfBound exception which is the next block
+
+        System.out.println("bye");
+    }
+}
